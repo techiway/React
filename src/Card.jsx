bcee7ab5c9f6d7c4ee5ea41a085a4db2b8031836
ManishRaghavan <manish.mahendran9499@gmail.com>
2021-09-27 14:31:20 +0530

    added product card

--- src/Card.jsx
+++ src/Card.jsx
@@ -13,15 +13,79 @@ export const Card = ({ img, inStore, discountVal ,discount }) => (
 );
 
 export const Card1 = ({ img, inStore, price ,size }) => (
-  <Box img={img}>
-    {inStore ? <Header> In-Store </Header> : null}
-    {/* {discount ? <Discount>${discountVal}</Discount> : null} */}
-    <Title>
-      {" "}
-      <p className="m-l">Price: ${price}</p> <p className="m-r">Size: {size}</p>
-    </Title>
-  </Box>
+  // <Box img={img}>
+  //   {inStore ? <Header> In-Store </Header> : null}
+  //   {/* {discount ? <Discount>${discountVal}</Discount> : null} */}
+  //   <Title>
+  //     {" "}
+  //     <p className="m-l">Price: ${price}</p> <p className="m-r">Size: {size}</p>
+  //   </Title>
+  // </Box>
+  <Box2>
+    <Layer1 src={img} alt='pics'/>
+    <Layer2>
+    <p>Mens Pant</p>
+    <p>Available Size :</p>
+    <SizeBoxContainer>
+  <SizeBox>
+    <p style={{marginTop:'10px'}}>S</p>
+  </SizeBox>
+  <SizeBox>
+    <p style={{marginTop:'10px'}}>S</p>
+  </SizeBox>
+  <SizeBox>
+    <p style={{marginTop:'10px'}}>S</p>
+  </SizeBox>
+  <SizeBox>
+    <p style={{marginTop:'10px'}}>S</p>
+  </SizeBox>
+    </SizeBoxContainer>
+    <p>Price -₹ 122</p>
+    </Layer2>
+  </Box2>
 );
+
+const Box2 = styled.div`
+  height: 350px;
+  width: 450px;
+  margin: 20px;
+  position: relative;
+  border-radius: 14px;
+  display: flex;
+  flex-direction:column;
+  `;
+const Layer1 = styled.img`
+  height:100%;
+  width:250px;
+  border-radius:6px 0 0 6px;
+`
+const Layer2 = styled.div`
+ height:100%;
+  width:200px;
+  border: 1px solid black;
+  background-color: #ccc;
+  border-radius:0 6px 6px 0;
+  display:flex;
+  flex-direction:column;
+  align-items:flex-start;
+`
+const SizeBoxContainer = styled.div`
+height:100px;
+ width:90%;
+  /* display:flex;
+ justify-content:space-between; */
+`
+
+const SizeBox = styled.div`
+height:40px;
+ width:40px;
+ border:1px solid black;
+ border-radius:50%;
+ display:flex;
+ float:left;
+`
+
+
 
 const Box = styled.div`
   height: 250px;
